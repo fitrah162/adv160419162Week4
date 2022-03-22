55a1cd401386bbfdc1e1e9ba4ae3c904260acf76
FITRAH162 <s160419162@student.ubaya.ac.id>
2022-03-23 05:35:50 +0700

    week 5

--- app/src/main/java/com/ubaya/adv160419162week4/model/Model.kt
+++ app/src/main/java/com/ubaya/adv160419162week4/model/Model.kt
@@ -1,7 +1,12 @@
 package com.ubaya.adv160419162week4.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Student(var id: String?,
+                   @SerializedName("student_name")
                    var name: String?,
+                   @SerializedName("birth_of_date")
                     var dob: String?,
                     var phone: String?,
+                   @SerializedName("photo_url")
                     var photoUrl: String?)
